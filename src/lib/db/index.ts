54bec19b652808649ab1a7ff586d6124ef07deb3
Cullen Abbs <cullen0880@gmail.com>
2023-04-23 17:33:30 -0400

    removed websockets

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,7 +1,5 @@
-import ws from "ws";
-import { Pool, neonConfig } from "@neondatabase/serverless";
+import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
-neonConfig.webSocketConstructor = ws;
 
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL
